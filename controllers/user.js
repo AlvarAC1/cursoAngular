12bad30b1c5d11306ad217cc2abe86de29d07df3
Alvar <valenciawalas@hotmail.com>
2020-02-03 18:30:44 +0100

    fin tema 21: Middleware de autenticación

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ var jwt = require('../services/jwt');
 
 function pruebas(req, res){
 	res.status(200).send({
-		message: 'Probando una acción del controlador de usuarios  del api rest con Node y Mongo'
+		message: 'Probando una acción del controlador de usuarios del api rest con Node y Mongo'
 	});
 }
 
--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,11 @@ var UserController = require('../controllers/user');
 //cargar el router expres en una variable para poder cargar rutas
 var api = express.Router();
 
+//cargar el middleware
+var md_auth = require('../middlewares/authenticated');
+
 //Cargar ruta
-api.get('/probando-controlador', UserController.pruebas);
+api.get('/probando-controlador', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
