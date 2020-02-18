3af001b1d24a898f52d4fa29cc89dc188835d51d
Alvar <valenciawalas@hotmail.com>
2020-02-18 13:00:44 +0100

    fin tema 32: Controlador y conseguir un album

---
+++ routes/album.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+var AlbumController = require('../controllers/album');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/album'});
+
+api.get('/album', md_auth.ensureAuth, AlbumController.getAlbum);
+
+
+// para poder usarlo fuera de este fichero
+module.exports = api;
