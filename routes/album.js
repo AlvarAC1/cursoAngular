6ac2aea0fe18d4e9f760e5744dfe05fdc0a1503f
Alvar <valenciawalas@hotmail.com>
2020-02-18 13:14:28 +0100

    fin tema 33: Guardar un album

--- routes/album.js
+++ routes/album.js
@@ -9,6 +9,8 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/album'});
 
 api.get('/album', md_auth.ensureAuth, AlbumController.getAlbum);
+api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
+
 
 
 // para poder usarlo fuera de este fichero
