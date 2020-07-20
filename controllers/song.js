5e0dd82f58e1405a631464c694e13c82af4996c7
Alvar <valenciawalas@hotmail.com>
2020-07-20 18:53:24 +0200

    fin tema 40+41: Accion para crear canciones dentro de album - Conseguir una cancion

--- controllers/song.js
+++ controllers/song.js
@@ -9,9 +9,45 @@ var Album = require('../models/album');
 var Song = require('../models/song');
 
 function getSong(req, res){
-	res.status(200).send({message: 'Controlador cancion'});
+	var songId = req.params.id;
+
+	Song.findById(songId).populate({path: 'album'}).exec((err, song) => {
+		if(err){
+			res.status(500).send({message: 'Error en la petición'});
+		}else{
+			if(!song){
+				res.status(404).send({message: 'La canción no existe !!'});
+			}else{
+				res.status(200).send({song});
+			}
+		}
+	});
+}
+
+function saveSong(req, res){
+	var song = new Song();
+
+	var params = req.body;
+	song.number = params.number;
+	song.name  = params.name;
+	song.duration = params.duration;
+	song.file = null;
+	song.album = params.album;
+
+	song.save((err, songStored) => {
+		if(err){
+			res.status(500).send({message: 'Error en el servidor'});
+		}else{
+			if(!songStored){
+				res.status(404).send({message: 'No se ha guardado la canción'});
+			}else{
+				res.status(200).send({song: songStored});
+			}
+		}
+	});
 }
 
 module.exports = {
-	getSong
-};
+	getSong,
+	saveSong
+}; 
