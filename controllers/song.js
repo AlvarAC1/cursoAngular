c53218a671d2c329653e2205fe3dc757c17a0eff
Alvar <valenciawalas@hotmail.com>
2020-07-21 21:48:12 +0200

    fin tema 45: Subir ficheros de audio

--- controllers/song.js
+++ controllers/song.js
@@ -108,11 +108,56 @@ function deleteSong(req, res){
 	});
 }
 
+function uploadFile(req, res){
+	var songId = req.params.id;
+	var file_name = 'No subido...';
+
+	if(req.files){
+		var file_path = req.files.file.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if(file_ext == 'mp3' || file_ext == 'ogg'){
+
+			Song.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdated) => {
+				if(!songUpdated){
+					res.status(404).send({message: 'No se ha podido actualizar la cación'});
+				}else{
+					res.status(200).send({song: songUpdated});
+				}
+			});
+
+		}else{
+			res.status(200).send({message: 'Extensión del archivo no valida'});
+		}
+
+	}else{
+		res.status(200).send({message: 'No has subido el fichero de audio...'});
+	}
+}
+
+function getSongFile(req, res){
+	var imageFile = req.params.songFile;
+	var path_file = './uploads/songs/'+imageFile;
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe el fichero de audio...'});
+		}
+	});
+}
+
 module.exports = {
 	getSong,
 	getSongs,
 	saveSong,
 	updateSong,
-	deleteSong
+	deleteSong,
+	uploadFile,
+	getSongFile
 
 }; 
